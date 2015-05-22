1c407bab34fb1726c7ec051674238d0c646dfe6c
Rodrigo Mendonça <rodrigo-mendonca@outlook.com.br>
2015-05-22 19:00:06 -0300

    Commit automatico

--- C/QuaseOrdena/main.c
+++ C/QuaseOrdena/main.c
@@ -4,10 +4,14 @@
 
 #define TAM 10;
 
+void list_print(int *);
+void list_ordenar(int *);
+int list_confere_ordem(int *);
+void list_posicao(int);
+void list_trocar(int*,int,int);
+void troca(int *,int,int);
 
-void print_list(int *);
-void ordenar_list(int *);
-void confere_ordem_list(int *);
+int Speed = 100;
 
 int main()
 {
@@ -16,44 +20,100 @@ int main()
 
     int i;
     for(i = 0;i<Tam;i++)
-        List[i] = i;
+        List[i] = Tam - i;
 
-    for(i = 0;i< Tam ;i++)
-    {
-        print_list(List);
-        //printf(" 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11| 12|\n");
-        printf("%c",32);
-
-        int j;
-        for(j = 1;j<=i*4;j++)
-            printf("%c",32);
-
-        printf("%c",24);
-
-        Sleep(500);
-        system("cls");
-    }
+    list_ordenar(List);
 
     return 0;
 }
 
-void print_list(int *List)
+void list_print(int *List)
 {
     int Tam = TAM;
     int i;
     for(i=0;i<Tam;i++)
     {
-        printf(" %i |",List[i]);
+        char str[15];
+        sprintf(str, "%d", List[i]);
+
+        printf("%3s|",str);
     }
     printf("\n");
 }
 
-void ordenar_list(int *List)
+int list_confere_ordem(int *List)
 {
+    int Tam = TAM;
+    int i;
+    int Erro = 0;
+    for(i=1;i<Tam;i++)
+    {
+        system("cls");
+        list_print(List);
+        //printf(" 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11| 12|\n");
+        list_posicao(i);
 
+        Sleep(Speed);
+
+        if(List[i-1] > List[i])
+        {
+            Erro = 1;
+            break;
+        }
+    }
+
+    if(Erro)
+        printf("\nNao Ordenado!");
+    else
+        printf("\nOrdenado!");
+    Sleep(Speed*3);
+
+    return(Erro);
 }
 
-void confere_ordem_list(int *List)
+void list_ordenar(int *List)
 {
+    int Tam = TAM;
+    int i,j;
 
+    for(i = 0;i< Tam ;i++)
+    {
+        for(j = 1;j<(Tam - i);j++)
+        {
+            list_confere_ordem(List);
+            list_trocar(List,j-1,j);
+        }
+    }
+}
+
+void list_posicao(int Pos)
+{
+    printf("%c",32);
+    int j;
+    for(j = 1;j<= Pos*4;j++)
+        printf("%c",32);
+
+    printf("%c",24);
+}
+
+void list_trocar(int *List,int Ini,int Fim)
+{
+    if(Ini == Fim || Ini > Fim)
+        return;
+
+    int i,I1,I2;
+    int Tam = (int)1 + (Fim - Ini) / 2;
+    for(i=0;i<Tam;i++)
+    {
+        I1 = i + Ini;
+        I2 = Fim - i;
+        troca(List,I1,I2);
+    }
+}
+
+void troca(int *List,int ind1,int ind2)
+{
+    int Aux = List[ind1];
+    List[ind1] = List[ind2];
+    List[ind2] = Aux;
 }
