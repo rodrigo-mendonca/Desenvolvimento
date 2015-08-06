bc07e1796868e08a7de0c73c5c3e92d0c3b0c385
rodrigo-mendonca <rodrigo-mendonca@outlook.com.br>
2015-08-06 19:00:07 -0300

    Commit automatico

--- CSharp/USBBackup/USBBackup/USBBackup.cs
+++ CSharp/USBBackup/USBBackup/USBBackup.cs
@@ -31,8 +31,11 @@ namespace USBBackup
 
         public void Backup()
         {
-            DirectoryCopy(Origin.FullName, Destiny.FullName, true);
-        
+            if (Directory.Exists(Destiny.FullName))
+            {
+                DirectoryCopy(Origin.FullName, Destiny.FullName, true);
+                DirectoryDelete(Destiny.FullName, Origin.FullName, true);
+            }
         }
 
         private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
@@ -59,7 +62,31 @@ namespace USBBackup
             foreach (FileInfo file in files)
             {
                 string temppath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(temppath, false);
+
+                if (File.Exists(temppath))
+                {
+                    DateTime filetemp = File.GetLastWriteTime(temppath);
+                    if (file.LastWriteTime > filetemp)
+                    {
+                        try
+                        {
+                            file.CopyTo(temppath, true);
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        file.CopyTo(temppath, false);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
             }
 
             // If copying subdirectories, copy them and their contents to new location. 
@@ -69,6 +96,61 @@ namespace USBBackup
                 {
                     string temppath = Path.Combine(destDirName, subdir.Name);
                     DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        private static void DirectoryDelete(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(sourceDirName))
+            {
+                return;
+            }
+
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+
+                if (!File.Exists(temppath))
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+
+                    if (!Directory.Exists(temppath))
+                    {
+                        try
+                        {
+                            subdir.Delete(true);
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+
+                    if (Directory.Exists(subdir.FullName))
+                        DirectoryDelete(subdir.FullName, temppath, copySubDirs);
                 }
             }
         }
