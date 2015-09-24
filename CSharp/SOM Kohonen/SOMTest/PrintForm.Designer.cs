1ed8303b6a238d5c73ba882be32ef9297a7df4de
Rodrigo Mendonça da Paixão <rodrigo-mendonca@outlook.com.br>
2015-09-24 20:41:05 -0300

    Exemplo de SOM

---
+++ CSharp/SOM Kohonen/SOMTest/PrintForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SOMTest
+{
+    partial class PrintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cmdTreinar = new System.Windows.Forms.Button();
+            this.NumTreinamento = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTreinamento)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(289, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cmdTreinar
+            // 
+            this.cmdTreinar.Location = new System.Drawing.Point(472, 255);
+            this.cmdTreinar.Name = "cmdTreinar";
+            this.cmdTreinar.Size = new System.Drawing.Size(75, 23);
+            this.cmdTreinar.TabIndex = 2;
+            this.cmdTreinar.Text = "Treinar";
+            this.cmdTreinar.UseVisualStyleBackColor = true;
+            this.cmdTreinar.Click += new System.EventHandler(this.cmdTreinar_Click);
+            // 
+            // NumTreinamento
+            // 
+            this.NumTreinamento.Location = new System.Drawing.Point(346, 258);
+            this.NumTreinamento.Name = "NumTreinamento";
+            this.NumTreinamento.Size = new System.Drawing.Size(120, 20);
+            this.NumTreinamento.TabIndex = 3;
+            this.NumTreinamento.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // PrintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 286);
+            this.Controls.Add(this.NumTreinamento);
+            this.Controls.Add(this.cmdTreinar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PrintForm";
+            this.Text = "PrintForm";
+            this.Load += new System.EventHandler(this.PrintForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTreinamento)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button cmdTreinar;
+        private System.Windows.Forms.NumericUpDown NumTreinamento;
+    }
+}
